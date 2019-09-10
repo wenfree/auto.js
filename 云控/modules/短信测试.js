b5c46d548a301ca595c17028c473518b0672e716
ouwen000 <wenhong520@qq.com>
2019-09-10 15:01:53 +0800

    update

---
+++ 云控/modules/短信测试.js
@@ -0,0 +1,30 @@
+importClass(android.content.ContentResolver);
+importClass(android.database.Cursor);
+importClass(android.net.Uri);
+
+//uri
+function get_sms_by_time(name,timeline){
+    var smsUri = "content://sms/inbox";
+    function xxxx( body ,date){
+        var sms_arr ={};
+        var cursor=context.getContentResolver().query(Uri.parse(smsUri), ["body"], "body like ? and date > ?",["%"+body+"%",date], "date desc");
+        if (cursor != null) {
+            let i=0;
+            while(cursor.moveToNext()){
+                var sms_content = cursor.getString(cursor.getColumnIndex("body")); 
+                console.log("短信", sms_content);
+                sms_arr[i]=sms_content;
+                i++
+            }
+        }
+        return sms_arr;
+    } 
+    if(!timeline){
+        timeline = 0;
+    }
+    return xxxx(name,timeline);
+}
+var datetime =new Date().getTime();
+log(datetime)
+
+log(get_sms_by_time("米",datetime -60*60*13*1000));
